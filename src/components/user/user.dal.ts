a2180c8a6a871e3e1ed0f9760d2c8f4179a90c72
AdityaKG-169 <aditya.kg169@gmail.com>
2021-11-01 22:57:29 +0530

    feat: Create user login functions
    created functions to check if the user is existing and to create new user

---
+++ src/components/user/user.dal.ts
@@ -0,0 +1,101 @@
+import User from './user.model';
+import responseHandler from '../../utilities/responseHandler';
+import { IResponse } from '../../types/IResponse';
+import { IResponseParams } from '../../types/IResponseParams';
+import { validateEmail, validateFullName } from '../../helpers/validation';
+
+const checkIfUserExists = async (contextObject: {
+	email: string;
+}): Promise<IResponse> => {
+	const { email } = contextObject;
+
+	const emailValidationResult: IResponse = validateEmail({ email });
+	if (emailValidationResult.data.type === 'error') return emailValidationResult;
+
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			const responseObj: IResponseParams = {
+				statusCode: 'NOT_FOUND',
+				data: { type: 'message' },
+				functionName: 'checkIfUserExists',
+				message: 'User is not present',
+				uniqueCode: 'user_not_present',
+			};
+			const messageObject: IResponse = responseHandler(responseObj);
+			return messageObject;
+		}
+		const responseObj: IResponseParams = {
+			statusCode: 'SUCCESS',
+			data: { type: 'message', payload: user },
+			functionName: 'checkIfUserExists',
+			message: 'User already exists',
+			uniqueCode: 'user_present',
+		};
+		const messageObject: IResponse = responseHandler(responseObj);
+		return messageObject;
+	} catch (err: unknown) {
+		const responseObj: IResponseParams = {
+			statusCode: 'INTERNAL_SERVER_ERROR',
+			data: { type: 'error', payload: err },
+			functionName: 'checkIfUserExists',
+			message: null,
+			uniqueCode: 'checkIfUserExists_internal_server_error',
+		};
+		const errorObject: IResponse = responseHandler(responseObj);
+		return errorObject;
+	}
+};
+
+const createUser = async (contextObject: {
+	email: string;
+	fullName: string;
+}): Promise<IResponse> => {
+	const { email, fullName } = contextObject;
+
+	const emailValidationResult: IResponse = validateEmail({ email });
+	if (emailValidationResult.data.type === 'error') return emailValidationResult;
+
+	const fullNameValidationResult: IResponse = validateFullName({ fullName });
+	if (fullNameValidationResult.data.type === 'error')
+		return fullNameValidationResult;
+
+	try {
+		const isUserPresent: IResponse = await checkIfUserExists({ email });
+		if (isUserPresent.uniqueCode !== 'user_not_present') return isUserPresent;
+
+		const user = await User.create({ email, fullName });
+		if (!user) {
+			const responseObj: IResponseParams = {
+				statusCode: 'INTERNAL_SERVER_ERROR',
+				data: { type: 'error' },
+				functionName: 'createUser',
+				message: 'user not created due to some mongo error',
+				uniqueCode: 'createUser_internal_server_error_0',
+			};
+			const errorObject: IResponse = responseHandler(responseObj);
+			return errorObject;
+		}
+		const responseObj: IResponseParams = {
+			statusCode: 'CREATED',
+			data: { type: 'success', payload: user },
+			functionName: 'createUser',
+			message: 'User created successfully',
+			uniqueCode: 'user_created',
+		};
+		const messageObject: IResponse = responseHandler(responseObj);
+		return messageObject;
+	} catch (err: unknown) {
+		const responseObj: IResponseParams = {
+			statusCode: 'INTERNAL_SERVER_ERROR',
+			data: { type: 'error', payload: err },
+			functionName: 'createUser',
+			message: null,
+			uniqueCode: 'createUser_internal_server_error_1',
+		};
+		const errorObject: IResponse = responseHandler(responseObj);
+		return errorObject;
+	}
+};
+
+export { checkIfUserExists, createUser };
