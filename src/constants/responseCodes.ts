f050f678dbe1d1dfe8824cbb24a142dd531894fa
AdityaKG-169 <aditya.kg169@gmail.com>
2021-11-02 05:02:26 +0530

    feat: Add file to handle response codes constants

---
+++ src/constants/responseCodes.ts
@@ -0,0 +1,12 @@
+const responseCodes = {
+	success: 'SUCCESS',
+	created: 'CREATED',
+	bad: 'BAD',
+	unauthorized: 'UNAUTHORIZED',
+	forbidden: 'FORBIDDEN',
+	notFound: 'NOT_FOUND',
+	unprocessable: 'UNPROCESSABLE',
+	internalServerError: 'INTERNAL_SERVER_ERROR',
+};
+
+export default responseCodes;
