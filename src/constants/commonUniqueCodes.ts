fc7d225d3bfba1816d6eacef664899ea3d6d3a62
AdityaKG-169 <aditya.kg169@gmail.com>
2021-11-04 08:23:49 +0530

    fix: seperated unique codes from all files.
    converted common unique codes into object

---
+++ src/constants/commonUniqueCodes.ts
@@ -0,0 +1,25 @@
+const uniqueCode = {
+	unauthorizedOrigin: 'unauthorized_origin',
+	unauthorizedRequest: 'unauthorized_request',
+	noEmailPresent: 'no_email',
+	invalidEmail: 'invalid_email',
+	validEmail: 'valid_email',
+	noFullNamePresent: 'no_full_name',
+	invalidFullName: 'invalid_full_name',
+	validFullName: 'valid_full_name',
+	noAppNamePresent: 'no_app_name',
+	invalidAppName: 'invalid_app_name',
+	validAppName: 'valid_app_name',
+	noAppDescriptionPresent: 'no_app_description',
+	invalidAppDescription: 'invalid_app_description',
+	validAppDescription: 'valid_app_description',
+	noUserIdPresent: 'no_user_id',
+	validUserId: 'valid_user_id',
+	nonExistingUser: 'user_not_existing',
+	userExists: 'user_exists',
+	userCreated: 'user_created',
+	userFetched: 'user_fetched',
+	internalServerError: 'internal_server_error',
+};
+
+export default uniqueCode;
