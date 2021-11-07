146ed58191d9a0d363a5364a5d336a4d5ab9764a
AdityaKG-169 <aditya.kg169@gmail.com>
2021-11-08 02:39:21 +0530

    fix: updated the function with unique codes

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -2,18 +2,19 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { checkIfUserExists } from '../components/user/user.dal';
 import config from '../config/config';
+import uniqueCode from '../constants/commonUniqueCodes';
 import responseCodes from '../constants/responseCodes';
 import { IResponse } from '../types/IResponse';
 import { IResponseParams } from '../types/IResponseParams';
 import responseHandler from '../utilities/responseHandler';
 
-const unauthorizedMessage = (count: number): IResponseParams => {
+const unauthorizedMessage = (): IResponseParams => {
 	const errorResponse: IResponseParams = {
 		statusCode: responseCodes.unauthorized,
 		functionName: 'verifyToken',
 		message: 'Unauthorized',
 		data: { type: 'error', payload: null },
-		uniqueCode: `verifyToken_err_not_authorized_${count}`,
+		uniqueCode: uniqueCode.unauthorizedRequest,
 	};
 	return errorResponse;
 };
@@ -28,24 +29,24 @@ const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
 		!authorization.startsWith('Bearer ') ||
 		!authorization.trim()
 	) {
-		const errorResponse: IResponseParams = unauthorizedMessage(0);
+		const errorResponse: IResponseParams = unauthorizedMessage();
 		const response: IResponse = responseHandler(errorResponse);
-		return res.status(response.status).json({ message: response });
+		return res.status(response.status).json({ response });
 	}
 
 	const token = authorization.split(' ')[1];
 
-	jwt.verify(token, JWT_SECRET, async (err, decoded) => {
+	return jwt.verify(token, JWT_SECRET, async (err, decoded) => {
 		if (err) {
-			const errorResponse: IResponseParams = unauthorizedMessage(1);
+			const errorResponse: IResponseParams = unauthorizedMessage();
 			const response: IResponse = responseHandler(errorResponse);
-			return res.status(response.status).json({ message: response });
+			return res.status(response.status).json({ response });
 		}
 
 		if (!decoded) {
-			const errorResponse: IResponseParams = unauthorizedMessage(2);
+			const errorResponse: IResponseParams = unauthorizedMessage();
 			const response: IResponse = responseHandler(errorResponse);
-			return res.status(response.status).json({ message: response });
+			return res.status(response.status).json({ response });
 		}
 
 		const userEmail: string = decoded.email;
@@ -55,18 +56,15 @@ const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
 		});
 
 		if (loggedInUser.data.type !== 'success') {
-			const errorResponse: IResponseParams = unauthorizedMessage(3);
+			const errorResponse: IResponseParams = unauthorizedMessage();
 			const response: IResponse = responseHandler(errorResponse);
-			return res.status(response.status).json({ message: response });
+			return res.status(response.status).json({ response });
 		}
 
-		res.locals.user = loggedInUser;
+		res.locals.user = loggedInUser.data.payload;
+
 		return next();
 	});
-
-	const errorResponse: IResponseParams = unauthorizedMessage(4);
-	const response: IResponse = responseHandler(errorResponse);
-	return res.status(response.status).json({ message: response });
 };
 
 export default verifyToken;
